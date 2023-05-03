0755a5717fbe547171fb18607144bf50e0c2546c
Jeevan Kandel <41426372+jivankandel@users.noreply.github.com>
2023-05-03 17:24:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup,find_packages
+with open('README.md') as f:
+    readme=f.read()
+with open('LICENSE') as f:
+    license=f.read()
 setup(
     name='puresnet',
     version='0.0.1',
-    description='pdb to sparse',
+    description='PUResNetV2.0 Prediction of Protein Ligand Binding Sites',
+    long_description=readme,
+    license=license.
     packages=find_packages(),
     install_requires=[
       'numpy'  
